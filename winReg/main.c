27a9f3615683be47b1c209afd5e114bbdf137209
root <root@localhost.localdomain>
2016-11-28 03:41:26 +0000

    C é só nojento... nojento...

--- winReg/main.c
+++ winReg/main.c
@@ -30,50 +30,53 @@ void prompt(){
     printf(" > ");
 }
 
+
+int clean_stdin()
+{
+    while (getchar()!='\n');
+    return 1;
+}
+
 int main(int argc, char *argv[])
 {
     int command = 0;
+	char c; 	
     greeting();
     while(1){
         menu();
         prompt();
-        scanf("%d", &command);
+        while((scanf(" %d%c", &command, &c)!=2 || c!='\n') && clean_stdin()){} 
+	//while ((c = getchar()) != '\n' && c != EOF) { } //DO NOT take this line
 	printf("\n");
         switch(command){
-            case 0 :
+            case 0:
                 printf("print help file\n");
                 break;
             case 1 :
                 //extractOS("SYSTEM");
                 break;
             case 2 :
-
                 break;
             case 3 :
-
                 break;
             case 4 :
-
                 break;
             case 5 :
-
                 break;
             case 6 :
-
                 break;
 	    case 7 :
-
                 break;
 	    case 8 :	
-
                 break;
 	    case 9 :
-
                 break;
-	    case 98 :
+	    case 97 :
+		break;
+	    case 98:
 		printf("xTractor is a post-mortem forensic tool that will extract certain data from windows registry of a ntfs file system image given as imput. It was develop by a team of three young IT engineers as a subject of Ciber Security Forensic course.\n");
                 break;
-	    case 99 :
+	    case 99:
 		printf("Thank you for using xTractor!\n");
 		exit(0);
                 break;
