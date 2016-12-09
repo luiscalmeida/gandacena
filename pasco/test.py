4a45b54d5bd1d787ca45489cdbdfcad93dda1085
pedro95gomes <pedro1995gomes@gmail.com>
2016-12-09 20:09:19 +0000

    new structure

--- pasco/test.py
+++
@@ -1,6 +0,0 @@
-import pascohelper
-r = pascohelper.iterparse('/tmp/tmpr41Td2/support.CITRIX/AppData/Local/Microsoft/Windows/Temporary Internet Files/Content.IE5/index.dat')
-
-for x in r:
-    if x:
-        print x
